eed1103e51e2c924636dbac8a5635852cdd7ad42
cwhite07 <44276093+cwhite07@users.noreply.github.com>
2018-10-23 17:07:50 -0500

    Update TFIDF.java

--- PS4/src/TFIDF.java
+++ PS4/src/TFIDF.java
@@ -4,37 +4,43 @@ public class TFIDF {
 
 	public double tf(  ArrayList<String> words, String word ) throws Exception {
 		
-		int count = 0;
+		double count = 0;
 		for ( String w : words ) {
 			if(word.equalsIgnoreCase(w)) {
 				count++;
 			}
 		}	
-		
-		return count / words.size();
+		return (double) ( count / words.size() );
 	}
 	
 	public double idf( ArrayList<String> positiveList, ArrayList<String> negativeList, ArrayList<String> neutralList, String word ) throws Exception {
-		int count = 0;
+		double count = 0;
+		
 		if ( positiveList.contains(word) ) {
 			count++;
-		}
+		}		
 		if ( negativeList.contains(word) ) {
 			count++;
 		}
 		if ( neutralList.contains(word) ) {
 			count++;
 		}
-		return 	Math.log(3 / count);
+		return (double) Math.log( 3 / count);
 	}
 	
 	public double tfidf(ArrayList<String> positiveList, ArrayList<String> negativeList,ArrayList<String> neutralList, String w, int emotion) throws Exception {
 		if(emotion == -1) {
-			return tf(negativeList, w) * idf(positiveList, negativeList, neutralList, w);	
+			
+			return tf(negativeList, w) * idf(positiveList, negativeList, neutralList, w);
+			
 		} else if(emotion == 0) {
-			return tf(neutralList, w) * idf(positiveList, negativeList, neutralList, w);	
+			
+			return tf(neutralList, w) * idf(positiveList, negativeList, neutralList, w);
+			
 		} else {
-			return tf(positiveList, w) * idf(positiveList, negativeList, neutralList, w);	
+			
+			return tf(positiveList, w) * idf(positiveList, negativeList, neutralList, w);
+			
 		}
 		
 		
