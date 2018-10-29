d3b993dfbc3793e9cc2649e70ce7bc439daf792f
cwhite07 <44276093+cwhite07@users.noreply.github.com>
2018-10-29 11:59:38 -0500

    Update runPreprocess.java

--- PS4/src/runPreprocess.java
+++ PS4/src/runPreprocess.java
@@ -9,29 +9,25 @@ public class runPreprocess {
 		File pos = new File("positive.txt");
 		File neg = new File("negative.txt");
 		File neutral = new File("neutral.txt");
-		ArrayList<String> positiveList = p.readFile(pos);
-		ArrayList<String> negativeList = p.readFile(neg);
-		ArrayList<String> neutralList = p.readFile(neutral);
+		ArrayList<ArrayList<String>> positiveList = p.readFile(pos);
+		ArrayList<ArrayList<String>> negativeList = p.readFile(neg);
+		ArrayList<ArrayList<String>> neutralList = p.readFile(neutral);
 		
 		p.positiveList = positiveList;
 		p.negativeList = negativeList;
 		p.neutralList = neutralList;
-		
-		for( String w : positiveList ) {
-			p.tfidf(w, 1);
-		}
-		
-		for( String w : negativeList ) {
-			p.tfidf(w, -1);
-		}
-		
-		for( String w : neutralList ) {
-			p.tfidf(w, 0);
-		}
+		p.tfidf(positiveList, 1);
+		p.tfidf(negativeList, -1);
+		p.tfidf(neutralList, 0);
 		
 		
 		p.loadFile();
+		Bayes bayes = new Bayes();
+		System.out.println(bayes.sentiment("I just tried the most amazing chocolate cake ever it was truly amazing really it was amazing"));
 		
+		
+		LogisticRegression lr = new LogisticRegression();
+		System.out.println(lr.sentiment("I was not offended by this cake quite the opposite actually"));
 	}
 
 }
